5d5b4a9a7e5e89237952de0ddf4d2ca37069ba21
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-04-12 18:27:07 +0300

    Changed dockerfile

--- WebUniversity/WebUniversity/obj/Debug/net6.0/WebUniversity.AssemblyInfo.cs
+++ WebUniversity/WebUniversity/obj/Debug/net6.0/WebUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11238d1a09dab95f576a5fbc1ce16aad92facec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01b44618553f5e86fdba01d33d2479d23811e25")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
